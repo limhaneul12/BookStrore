14d2c5cf68dfc0f4afb59ddf74d7e3bde8031026
sky81219 <=>
2023-05-23 03:37:07 +0900

    sytle: 주석

--- kyobo/apps/store/apis.py
+++ kyobo/apps/store/apis.py
@@ -1,5 +1,4 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser, AllowAny
@@ -10,17 +9,31 @@ from .serializer import *
 
 # Create your views here.
 class AuthorAbstractFilter:
+    """
+    filter abstract 
+    추상화 작성 pagenation 추가 고민 
+    """
     filters_backends = [DjangoFilterBackend]
     filters_fields = ["author"]
 
 
 class AllAuthorTotalInformationViewset(ModelViewSet, AuthorAbstractFilter):
+    """
+    Args:
+        ModelViewSet (_type_): CRUD
+        AuthorAbstractFilter (_type_): 필터 추상화 
+    """
     queryset = AuthorInformation.objects.all()
     serializer_class = AuthorBookInformationSerializer
     permission_classes = (IsAdminUser, )
 
 
 class PublicBookReslses(ListAPIView, AuthorAbstractFilter):
+    """
+    Args:
+        ListAPIView (_type_): 출시 완료된 도서만 
+        AuthorAbstractFilter (_type_): 필터 추상화 
+    """
     queryset = BooksAndPublicationYear.objects.filter(is_public=True) 
     serializer_class = BookInformationSerializer
     permission_classes = (AllowAny, )
